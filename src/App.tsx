86a34a70db2b32af8ffd03f9a757aa64db996be1
Eric-Philippe <ericphlpp@gmail.com>
2024-01-08 14:26:45 +0100

    Remove HashRouter from main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import DevPortfolio from "./pages/DevPortfolio";
 import PhotoPortfolio from "./pages/PhotoPortfolio";
 
