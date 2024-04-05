3316a55f9c1f07c26bf921366d985ade7adfc23d
Eric-Philippe <66321178+Eric-Philippe@users.noreply.github.com>
2024-04-05 09:07:51 +0200

    Update CV

--- src/utils/Links.ts
+++ src/utils/Links.ts
@@ -1,6 +1,6 @@
 const LINKS = {
   DL_CV:
-    "https://www.dropbox.com/scl/fi/vv7szpf3x1ax3kjuetowd/Eric-PHILIPPE-CV.pdf?rlkey=xci3flpmq8cnybghfq1npqojf&dl=1",
+    "https://www.dropbox.com/scl/fi/bxdr0pg95xa6wc0umuua9/Eric-PHILIPPE-CV.pdf?rlkey=f14hshjyp2xh5d3pyw189auob&dl=1",
   GITHUB: "https://github.com/Eric-Philippe",
   LINKEDIN: "https://www.linkedin.com/in/eric-philippe-800857252/",
   INSTAGRAM: "https://www.instagram.com/eir_horizon_?igsh=bnd3d3d2ZmhwajE1",
